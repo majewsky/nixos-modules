90ae0935efe31ad379e3f558c63135ba12bc1e32
Stefan Majewsky <majewsky@gmx.net>
2019-01-29 18:02:38 +0100

    platform-hetzner-cloud: enable haveged

--- platform-hetzner-cloud.nix
+++ platform-hetzner-cloud.nix
@@ -18,13 +18,18 @@ with lib; {
     };
 
     # imported from the auto-generated /etc/nixos/hardware-configuration.nix
-    boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "sd_mod" "sr_mod" ];
+    boot.initrd.availableKernelModules = [
+      "ata_piix" "uhci_hcd" "virtio_pci" "sd_mod" "sr_mod"
+    ];
     fileSystems."/" = {
       device = "/dev/sda1";
       fsType = "ext4";
     };
     swapDevices = [];
     nix.maxJobs = lib.mkDefault 1;
+
+    # misc
+    services.haveged.enable = true;
   };
 
 }
