25b0c15d9a58f5b100f080723827d33af5bb022d
Stefan Majewsky <majewsky@gmx.net>
2019-02-26 18:45:20 +0100

    enable IPv6 on Hetzner Cloud

--- platforms/hetzner-cloud.nix
+++ platforms/hetzner-cloud.nix
@@ -8,6 +8,14 @@ with lib; {
     <nixpkgs/nixos/modules/profiles/qemu-guest.nix>
   ];
 
+  options.my.hetzner-cloud = {
+    ipv6Address = mkOption {
+      description = "the IPv6 address for this machine (must end in ::1)";
+      example = "2001:0db8:dead:beef::1";
+      type = types.string;
+    };
+  };
+
   config = {
     # use Grub bootloader
     boot.loader.grub = {
@@ -26,6 +34,18 @@ with lib; {
     };
     swapDevices = [];
     nix.maxJobs = lib.mkDefault 1;
+
+    # network configuration: IPv4 comes via DHCP, but IPv6 must be given in via
+    # the config (could theoretically be retrieved from the metadata service,
+    # but we cannot use the network during nixos-rebuild)
+    networking.interfaces.ens3.ipv6.addresses = [{
+      address = config.my.hetzner-cloud.ipv6Address;
+      prefixLength = 64;
+    }];
+    networking.defaultGateway6 = {
+      address = "fe80::1";
+      interface = "ens3";
+    };
 
     # misc
     services.haveged.enable = true;
