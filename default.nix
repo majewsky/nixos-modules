d308137e94e9081659ea3c2b2ad76a10a99a640d
Stefan Majewsky <majewsky@gmx.net>
2019-07-12 18:00:06 +0200

    taiga.nix: setup NixOS container with Postgres

--- default.nix
+++ default.nix
@@ -51,6 +51,7 @@ in {
     ./prometheus.nix
     ./prosody.nix
     ./static-websites.nix
+    ./taiga.nix
     ./umurmur.nix
     ./vt6-website.nix
     /nix/my/unpacked/generated-basic.nix # supplies config.my.machineID and config.networking.hostname
---
+++ taiga.nix
@@ -0,0 +1,63 @@
+# This module deploys Taiga, a project management software.
+{ config, pkgs, lib, ... }:
+
+with lib;
+
+let
+
+  cfg = config.my.services.taiga;
+
+  internalBackendListenPort = 17695;
+
+in {
+
+  options.my.services.taiga = {
+    domainName = mkOption {
+      default = null;
+      description = "domain name for Taiga (must be given to enable the service)";
+      type = types.nullOr types.str;
+    };
+  };
+
+  config = mkIf (cfg.domainName != null) {
+
+    containers.taiga = {
+      autoStart = true;
+      forwardPorts = [{
+        containerPort = internalBackendListenPort;
+        hostPort = internalBackendListenPort;
+      }];
+      privateNetwork = true;
+      hostAddress = "192.168.100.1";
+      localAddress = "192.168.100.2";
+
+      config = {config, pkgs, lib, ... }: with lib; {
+
+        environment.systemPackages = with pkgs; [
+          postgresql_11 # for psql(1)
+        ];
+
+        users.groups.taiga.gid = 1000;
+        users.users.taiga = {
+          uid = 1000;
+          group = "taiga";
+          home = "/";
+          shell = pkgs.bashInteractive;
+        };
+
+        services.postgresql = {
+          enable = true;
+          dataDir = "/var/lib/postgresql";
+          package = pkgs.postgresql_11;
+          initialScript = pkgs.writeText "taiga-postgres-init.sql" ''
+            CREATE USER taiga;
+            CREATE DATABASE taiga OWNER taiga ENCODING 'utf-8' LC_COLLATE 'en_US.UTF-8' LC_CTYPE 'en_US.UTF-8' TEMPLATE template0;
+          '';
+        };
+
+      };
+    };
+
+  };
+
+}
