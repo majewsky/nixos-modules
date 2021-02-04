5f4451d5a1324af4821466f1f2955aca6b0df29f
Stefan Majewsky <majewsky@gmx.net>
2021-02-04 11:22:06 +0100

    bump Linux to 5.10

--- platforms/hetzner-cloud.nix
+++ platforms/hetzner-cloud.nix
@@ -19,7 +19,7 @@ with lib; {
   config = {
     # the current LTS kernel in 20.09 does not have the builtin Wireguard
     # module, so use a newer kernel that does
-    boot.kernelPackages = mkIf (config.system.stateVersion == "20.09") pkgs.linuxPackages_5_9;
+    boot.kernelPackages = mkIf (config.system.stateVersion == "20.09") pkgs.linuxPackages_5_10;
 
     # use Grub bootloader
     boot.loader.grub = {
