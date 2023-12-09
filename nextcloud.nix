6297493493ab772ffd24a1777897e6cf255a02f8
Stefan Majewsky <majewsky@gmx.net>
2023-12-09 13:16:56 +0100

    nextcloud: replace postgresql.ensureUsers with ensureDBOwnership
    By using the logic built into the upstream nextcloud module.

--- nextcloud.nix
+++ nextcloud.nix
@@ -30,39 +30,28 @@ in {
       # NOTE: the rest is configured by services.nextcloud.enable
     };
 
-    services.postgresql = {
-      package = pkgs.postgresql_15;
-      enable = true;
-      dataDir = "/var/lib/postgresql-15";
-      ensureDatabases = [ "nextcloud" ];
-      ensureUsers = [{
-        name = "nextcloud";
-        ensurePermissions."DATABASE nextcloud" = "ALL PRIVILEGES";
-      }];
-    };
-
-    # ensure that postgres is running *before* running the setup
-    systemd.services."nextcloud-setup" = {
-      requires = ["postgresql.service"];
-      after    = ["postgresql.service"];
-    };
-
     services.nextcloud = {
       enable = true;
       hostName = cfg.domainName;
       package = pkgs.nextcloud27;
 
+      database.createLocally = true;
+
       # NOTE: services.nextcloud.config is only used for the initial setup, afterwards Nextcloud's stateful config takes precedence
       config = {
         dbtype = "pgsql";
         dbuser = "nextcloud";
-        dbhost = "/run/postgresql";
         dbname = "nextcloud";
         adminpassFile = toString /var/lib/nextcloud-root-password;
         adminuser = "root";
         overwriteProtocol = "https";
         defaultPhoneRegion = "DE";
       };
+    };
+
+    services.postgresql = {
+      package = pkgs.postgresql_15;
+      dataDir = "/var/lib/postgresql-15";
     };
 
   };
