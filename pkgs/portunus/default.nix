a19c6fbd4b2a1f086b6ab66af441c27283861ee7
Stefan Majewsky <majewsky@gmx.net>
2023-12-27 10:05:45 +0100

    portunus-2.0.0

--- pkgs/portunus/default.nix
+++ pkgs/portunus/default.nix
@@ -6,13 +6,13 @@
 
 buildGoModule rec {
   pname = "portunus";
-  version = "2.0.0-beta.2";
+  version = "2.0.0";
 
   src = fetchFromGitHub {
     owner = "majewsky";
     repo = "portunus";
     rev = "v${version}";
-    sha256 = "sha256-1OU3bepvqriGCW1qDszPnUDJ6eqBzNTiBZ2J4KF4ynw=";
+    sha256 = "sha256-jicqH31Q+kDkOvtCg+HStQ4LUUzKm5ZO4utnAkCOLvY=";
   };
 
   buildInputs = [ libxcrypt-legacy ];
